ba611ec87cc774552e6639cd07d38d4a99e19d52
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2023-08-23 13:11:51 -0400

    Create stringExamples.py

---
+++ stringExamples.py
@@ -0,0 +1,56 @@
+# Example 1: Concatenating Two String Literals
+greeting = "Hello, " + "world!"
+print("Example 1:", greeting)  # Output: Hello, world!
+
+ 
+
+# Example 2: Basic String Concatenation
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print("Example 2:", full_name)  # Output: John Doe
+
+ 
+
+# Example 3: Concatenating String with Literal Text
+greeting = "Hello, " + full_name + "!"
+print("Example 3:", greeting)  # Output: Hello, John Doe!
+
+ 
+
+# Example 4: Concatenating String and Numbers
+age = 25
+message = "Your age is: " + str(age)  # Convert int to str for concatenation
+print("Example 4:", message)  # Output: Your age is: 25
+
+ 
+
+# Example 5: Combining String and User Input
+user_name = input("Enter your name: ")
+welcome = "Welcome, " + user_name + "!"
+print("Example 5:", welcome)
+
+ 
+
+# Example 6: Concatenating Multiple Strings
+subject = "Python"
+verb = "is"
+description = "awesome!"
+sentence = subject + " " + verb + " " + description
+print("Example 6:", sentence)
+
+ 
+
+# Example 7: String Multiplication (Creating Repeated Patterns)
+divider = "-" * 20
+print("Example 7:", divider)  # Output: --------------------
+
+ 
+
+# Example 8: String Multiplication with Text
+stars = "*" * 5
+border = "|" + stars + "|"
+print("Example 8:")
+print(border)
+print(border)
+print(border)
